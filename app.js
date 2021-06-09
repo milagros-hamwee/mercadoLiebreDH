b3e8bb46581590b7cadb5d2e103f2ff6a682b850
milagros-hamwee <milihamwee@gmail.com>
2021-06-08 23:41:49 -0300

    procfile y cambio de puerto

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ app.get("/login", (req, res)=> {
     res.sendFile(path.resolve(__dirname, "./views/login.html"))
 })
 
-app.listen (3001, ()=> {
-    console.log("funcionando")
+app.listen (process.env.PORT || 3000, function (){
+    console.log('Servidor corriendo en el puerto 3000')
 })
