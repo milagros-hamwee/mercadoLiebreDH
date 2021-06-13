fe0eca424949b185405cd726f64226b004086f86
milagros-hamwee <milihamwee@gmail.com>
2021-06-12 23:55:40 -0300

    banner corregido

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ app.get("/login", (req, res)=> {
     res.sendFile(path.resolve(__dirname, "./views/login.html"))
 })
 
-app.listen (process.env.PORT || 3000, function (){
-    console.log('Servidor corriendo en el puerto 3000')
+app.listen (process.env.PORT || 3001, function (){
+    console.log('Servidor corriendo en el puerto 3001')
 })
